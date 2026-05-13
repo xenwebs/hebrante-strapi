ce2723430f07ca4e1bd3ff82565858cbcd951c52
Iakov <Iakov@192.168.0.7>
2026-05-13 18:59:01 -0500

    Move generate-signature route to order.ts

--- src/api/order/routes/order.ts
+++ src/api/order/routes/order.ts
@@ -5,3 +5,15 @@
 import { factories } from '@strapi/strapi';
 
 export default factories.createCoreRouter('api::order.order');
+
+export const routes = [
+  {
+    method: 'post',
+    path: '/orders/generate-signature',
+    handler: 'api::order.controllers.bold.generateSignature',
+    config: {
+      auth: false,
+      policies: [],
+    },
+  },
+];
