4ffed8ed4311cc3d350ce122e73cf45db00caa1b
Iakov <Iakov@192.168.0.7>
2026-05-13 17:55:10 -0500

    Add Bold signature route and controller

--- src/api/order/routes/order.ts
+++ src/api/order/routes/order.ts
@@ -4,4 +4,25 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::order.order');
+export default factories.createCoreRouter('api::order.order', {
+  only: ['find', 'findOne', 'create', 'update', 'delete'],
+  config: {
+    create: {
+      auth: false,
+      policies: [],
+    },
+  },
+});
+
+// Добавь кастомный маршрут
+export const customRoutes = [
+  {
+    method: 'post',
+    path: '/orders/generate-signature',
+    handler: 'api::order.controllers.bold.generateSignature',
+    config: {
+      auth: false,
+      policies: [],
+    },
+  },
+];
