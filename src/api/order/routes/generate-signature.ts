ee5888180cf17f9676a59eb8b4c78b5d25410234
Iakov <Iakov@192.168.0.7>
2026-05-13 20:16:13 -0500

    Fix handler path in generate-signature route

--- src/api/order/routes/generate-signature.ts
+++ src/api/order/routes/generate-signature.ts
@@ -2,7 +2,7 @@ export default [
   {
     method: 'POST',
     path: '/orders/generate-bold-signature',
-    handler: 'api::order.controllers.bold.generateSignature',  // ← ПОЛНЫЙ путь!
+    handler: require('../controllers/bold').default.generateSignature,  // ← ПОЛНЫЙ путь!
     config: {
       auth: false,
     },
