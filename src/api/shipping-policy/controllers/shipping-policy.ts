bfa3e002b01a296adedc66b4521a5c1d1a4f5263
Iakov <Iakov@192.168.0.4>
2026-05-25 12:26:25 -0500

    Add shipping-policy content type with manual i18n

---
+++ src/api/shipping-policy/controllers/shipping-policy.ts
@@ -0,0 +1,7 @@
+/**
+ * shipping-policy controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::shipping-policy.shipping-policy');
---
+++ src/api/shipping-policy/services/shipping-policy.ts
@@ -0,0 +1,7 @@
+/**
+ * shipping-policy service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::shipping-policy.shipping-policy');
